b4e20d0b54b6941931ecccd3ba62a97415fb066a
ethan <preevete@gmail.com>
2020-11-23 23:49:36 -0700

    added pan CJK fonts so japanese shows up correctly

--- sub-scripts/install-software.sh
+++ sub-scripts/install-software.sh
@@ -15,6 +15,7 @@ sudo pacman -S --noconfirm xmonad xmonad-contrib xterm
 # install other useful programs
 echo "installing other programs, git, firefox etc"
 sudo pacman -S --noconfirm git firefox udisks2 ripgrep pavucontrol gnupg usbutils unzip nemo
+sudo pacman -S --noconfirm adobe-source-han-sans-otc-fonts
 
 # install termite
 echo "installing termite terminal emulator"
