746b2fcf3e5a193bd6f11bc6cd09b9b02bdcc2b3
ethan <preevete@gmail.com>
2021-02-19 00:40:43 -0700

    newSSHConnection testing

--- utility-scripts/newSSHConnection.sh
+++ utility-scripts/newSSHConnection.sh
@@ -1,4 +1,9 @@
 source ~/git/linux-bash-tools/libs/error-handling.shlib
 infocmp > termite.terminfo
-scp termite.terminfo \$1:
-ssh \$1 'tic -x termite.terminfo'
+ssh-keygen -N "" -t rsa -b 2048 -f $HOME/.ssh/terminfo-temp-id
+ssh-copy-id -i $HOME/.ssh/terminfo-temp-id $1
+
+scp -i $HOME/.ssh/terminfo-temp-id termite.terminfo $1:
+#scp -i $HOME/.ssh/terminfo-temp-id vim-setup.sh $1:
+ssh -i $HOME/.ssh/terminfo-temp-id $1 'tic -x termite.terminfo'
+#ssh -i $HOME/.ssh/terminfo-temp-id $1 'bash vim-setup.sh'
