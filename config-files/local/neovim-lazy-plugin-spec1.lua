a85dbc4c15cedb4ef7e210a5463351fcdafc6195
Elizabeth Wynn <ewynn@chegg.com>
2025-04-21 22:33:45 -0600

    refactored the plugins and added a colorscheme

--- config-files/local/neovim-lazy-plugin-spec1.lua
+++ config-files/local/neovim-lazy-plugin-spec1.lua
@@ -1,11 +1,46 @@
 return {
-	{
-    "smoka7/hop.nvim",
-    version = "*"
-  },
-
   {
     "ggandor/leap.nvim",
     version = "*"
-  }
+  },
+  { "ntk148v/habamax.nvim", dependencies={ "rktjmp/lush.nvim" } }
+--  {
+--    "nvim-treesitter/nvim-treesitter",
+--    opts = {
+--      -- A list of parser names, or "all" (the five listed parsers should always be installed)
+--      ensure_installed = { "lua", "vim", "markdown", "markdown_inline", "yaml", "python" },
+--      highlight = {
+--        enable = true,
+--
+--        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+--        disable = function(lang, buf)
+--          local max_filesize = 100 * 1024 -- 100 KB
+--          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+--          if ok and stats and stats.size > max_filesize then
+--            return true
+--          end
+--        end,
+--
+--        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+--        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+--        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+--        -- Instead of true it can also be a list of languages
+--        additional_vim_regex_highlighting = false,
+--      }
+--    }
+--  },
+--  {
+--    "windwp/nvim-ts-autotag",
+--    dependencies = { "nvim-treesitter/nvim-treesitter" },
+--    opts = {
+--      autotag = {
+--        enable = true,
+--        enable_rename = true,
+--        enable_close = true,
+--        enable_close_on_slash = true,
+--        filetypes = { "lua", "vim" },
+--      }
+--    }
+--  },
 }
+
--- config-files/local/nvim-init.lua
+++ config-files/local/nvim-init.lua
@@ -1,7 +1,4 @@
 vim.cmd([[
-  noremap K 10k
-  noremap H ^
-  noremap L $
   set expandtab
   set shiftwidth=2
   set tabstop=2
@@ -10,13 +7,80 @@ vim.cmd([[
   
   set smartcase
   set ignorecase
+  set smartindent
   syntax on
 ]])
 
 vim.keymap.set('i', 'jk', '<Esc>')
-vim.keymap.set("n", "J", "10j")
+vim.keymap.set({"n", "v"}, "J", "10j")
+vim.keymap.set({"n", "v"}, "K", "10k")
+vim.keymap.set({"n", "v"}, "H", "^")
+vim.keymap.set({"n", "v"}, "L", "$")
+
+-- indentation
+vim.o.tabstop = 2
+vim.o.shiftwidth = 0
+vim.o.softtabstop = -1
+vim.o.expandtab = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- More intuitive splits
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Line breaks
+vim.o.linebreak = true
+vim.o.breakindent = true
+vim.o.wrap = true
+
+-- space as a dot
+-- ,space:·
+-- multispace changes what the tabcharacter looks like
+-- this errors out for some reason the character won't display
+-- eol:\\u21B4,
+vim.o.list = true
+vim.o.listchars = "tab:| ,trail:+,precedes:<,extends:>,nbsp:␣,space: ,multispace:  ,"
+
+
+-- from u/caagr98 on reddit: https://www.reddit.com/r/neovim/comments/17aponn/comment/k5f2n7t/
+local function update_lead()
+  local lcs = vim.opt_local.listchars:get()
+  local tab = vim.fn.str2list(lcs.tab)
+  local space = vim.fn.str2list(lcs.multispace or lcs.space)
+  local lead = {tab[1]}
+  for i = 1, vim.bo.tabstop-1 do
+      lead[#lead+1] = space[i % #space + 1]
+  end
+  -- vim.o.listchars = "tab:» ,trail:+,precedes:<,extends:>,nbsp:␣,eol:↴,leadmultispace:" + vim.fn.list2str(lead)
+  -- vim.opt_local.listchars = vim.opt_local.listchars + 'leadmultispace:' + vim.fn.list2str(lead)
+  vim.opt_local.listchars:append({ leadmultispace = vim.fn.list2str(lead) })
+end
+
+vim.api.nvim_create_autocmd("OptionSet", { pattern = { "listchars", "tabstop", "filetype" }, callback = update_lead })
+vim.api.nvim_create_autocmd("VimEnter", { callback = update_lead, once = true })
 
 vim.wo.number = true
 
 require("config.lazy")
-require('leap').set_default_mappings()
+-- require('plugins.leap.nvim').set_default_mappings()
+
+vim.cmd("colorscheme habamax")
+
+vim.api.nvim_create_user_command('SyntaxHere', function()
+	local pos = vim.api.nvim_win_get_cursor(0)
+	local line, col = pos[1], pos[2] + 1
+	print 'Regex (legacy) Syntax Highlights'
+	print '--------------------------------'
+	print(' effective: ' .. vim.fn.synIDattr(vim.fn.synID(line, col, true), 'name'))
+	for _, synId in ipairs(vim.fn.synstack(line, col)) do
+		local synGroupId = vim.fn.synIDtrans(synId)
+		print(' ' .. vim.fn.synIDattr(synId, 'name') .. ' -> ' .. vim.fn.synIDattr(synGroupId, 'name'))
+	end
+	print ' '
+	print 'Tree-sitter Syntax Highlights'
+	print '--------------------------------'
+	vim.print(vim.treesitter.get_captures_at_cursor(0))
+end, {})
